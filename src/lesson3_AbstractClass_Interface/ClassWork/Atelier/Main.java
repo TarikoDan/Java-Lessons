9934a77feae3b707e72eafbcaeadb4cae6b54fb5
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-08-20 22:53:35 +0300

    Less3 -  completed

--- src/lesson3_AbstractClass_Interface/ClassWork/Atelier/Main.java
+++ src/lesson3_AbstractClass_Interface/ClassWork/Atelier/Main.java
@@ -12,14 +12,15 @@ public class Main {
         Dress dressSBlack = new Dress(Sizes.S, "black", 157.5);
         Dress dressMBlack = new Dress(Sizes.M, "black", 157.5);
         Dress dressLBlack = new Dress(Sizes.L, "black", 157.5);
-        Pents jensM =new Pents(Sizes.M, "blue", 43.0);
-        Pents jensMWhite =new Pents(Sizes.M, "white", 48.0);
-        Pents jensXL =new Pents(Sizes.XL, "blue", 43.0);
-        Pents jensLWhite =new Pents(Sizes.L, "white", 48.0);
+        Pents jensM = new Pents(Sizes.M, "blue", 43.0);
+        Pents jensMWhite = new Pents(Sizes.M, "white", 48.0);
+        Pents jensXL = new Pents(Sizes.XL, "blue", 43.0);
+        Pents jensLWhite = new Pents(Sizes.L, "white", 48.0);
         Tie narrowTie = new Tie(Sizes.M, "black", 117.4);
         Tie wideTie = new Tie(Sizes.M, "coloured", 24.4);
 
-        Atelier modaMarket = new Atelier(new Wear[]{dressLBlack,dressMBlack,dressSBlack,jensLWhite,jensM,jensMWhite,jensXL,narrowTie,tShirtMWhite,tShirtXSRed,wideTie});
+        Wear[] wearSet = new Wear[]{dressLBlack,dressMBlack,dressSBlack,jensLWhite,jensM,jensMWhite,jensXL,narrowTie,tShirtMWhite,tShirtXSRed,wideTie};
+        Atelier modaMarket = new Atelier(wearSet);
         System.out.println(modaMarket.optionMAN());
         System.out.println(modaMarket.optionWOMAN());
 
